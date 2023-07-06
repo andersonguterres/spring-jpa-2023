7ae2194b4a8cbe81608bb168a71c312e5a4d1906
andersonguterres <andersongutprogramador>
2023-07-06 18:52:20 -0300

    Projeto Criado

---
+++ src/main/java/com/example/projetospringjpa2023/ProjetoSpringJpa2023Application.java
@@ -0,0 +1,13 @@
+package com.example.projetospringjpa2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringJpa2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoSpringJpa2023Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projetospringjpa2023/ProjetoSpringJpa2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetospringjpa2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringJpa2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
